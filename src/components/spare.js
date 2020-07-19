c44ae74fc09f47580ae8f38de6084cf1c90e2004
Sethuram.S.V <58566745+Sethuram52001@users.noreply.github.com>
2020-07-19 10:50:23 +0530

    Add files via upload

---
+++ src/components/spare.js
@@ -0,0 +1,70 @@
+import React from 'react'; 
+
+const api_url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+
+class StatsDisplay extends React.Component {
+
+	constructor(props){
+		super(props);
+		console.log('constructor',props.data);
+	}
+
+	state={
+		currentPrice: null,
+		dailyChange: null,//24 hour change
+		dailyChangeP:null,
+		monthlyChange: null,//monthly change
+		monthlyChangeP: null,
+		lastUpdated: null
+	}
+
+	componentDidMount =()=>
+	{
+	  	console.log("StatsDisplay Mounted");
+	  	const {data}=this.props;
+	  	console.log('componet did mount:',data);
+    }
+
+	getData = () =>{
+		const {data} = this.props;
+		console.log('getData: ',data);
+      	//console.log(data[30].pValue);
+      	fetch(api_url).then(d => d.json()).then((Data) =>
+      	{
+      		console.log(Data);
+      		const current_price =  Data.bpi.USD.rate_float;
+      		const daily_change = current_price-data[30].pValue;
+      		const daily_changeP = (daily_change/data[30].pValue)*100;
+      		const monthly_change = current_price-data[0].pValue;
+      		const monthly_changeP = ((monthly_change)/data[0].pValue)*100;
+      		const last_updated = Data.time.updated;
+      		this.setState({
+      			currentPrice:current_price,
+      			dailyChange:daily_change.toLocaleString('us-EN',{style:'currency',currency:'USD'}),
+      			dailyChangeP:daily_changeP.toFixed(2)+'%',
+      			monthlyChange:monthly_change.toLocaleString('us-EN',{style:'currency',currency:'USD'}),
+      			monthlyChangeP:monthly_changeP.toFixed(2)+'%',
+      			lastUpdated:last_updated		 
+      		});
+      	}).catch((e)=>{console.log(e);});
+    }
+
+	render() {
+		return (
+			<div>
+				<button onClick={this.getData}>button</button>
+				<div className="StatsBar">
+					<p>current price: {this.state.currentPrice}</p>
+					<p>last updated: {this.state.lastUpdated}</p>
+					<p>daily change: {this.state.dailyChange}</p>
+					<p>daily change : {this.state.dailyChangeP}</p>
+					<p>monthly change: {this.state.monthlyChange}</p>
+					<p>monthly change : {this.state.monthlyChangeP}</p>
+					<p>last updated: {this.state.lastUpdated}</p>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default StatsDisplay;
